da1968a2d350a355e01d3b0225db4590fb547722
Sumedha Nagpal <sumedhan@usc.edu>
2016-04-19 03:25:07 -0700

    added hashmap for language count per mime type

--- LanguageAnalysis1.java
+++ LanguageAnalysis1.java
@@ -17,74 +17,84 @@ import org.apache.tika.language.*;
 
 import org.xml.sax.SAXException;
 
-public class LanguageAnalysis1 {
- 
-	public static String detectLanguage(String FilePath) throws IOException, SAXException, TikaException{
+public class LanguageAnalysis1
+{
+
+	public static String detectLanguage(String FilePath) throws IOException, SAXException, TikaException
+	{
 				
-		  String filePath = FilePath.replace("\\", "\\\\");
+		
+		Parser parser = new AutoDetectParser();
+	    BodyContentHandler handler = new BodyContentHandler();
+	    Metadata metadata = new Metadata();
+		LanguageIdentifier object = new LanguageIdentifier(handler.toString());
+		String filePath = FilePath.replace("\\", "\\\\");
+		File file = new File(filePath);
+		FileInputStream content = new FileInputStream(file);
+	    parser.parse(content, handler, metadata, new ParseContext());
+	    return object.getLanguage();
+	}
 
-		  File file = new File(filePath);
-
-	      //Parser method parameters
-	      Parser parser = new AutoDetectParser();
-	      BodyContentHandler handler = new BodyContentHandler();
-	      Metadata metadata = new Metadata();
-
-	      FileInputStream content = new FileInputStream(file);
-
-	      //Parsing the given document
-	      parser.parse(content, handler, metadata, new ParseContext());
-
-	      LanguageIdentifier object = new LanguageIdentifier(handler.toString());
-	      
-	      return object.getLanguage();
+	static HashMap<String, ArrayList<String>> languageMap = new HashMap<String, ArrayList<String>>();
 	
-}
-	
-	
-public static void writeJson() throws IOException{
+	public static void showFiles(File[] files)throws Exception
+	{
+		String language1 = null;
+		
+		for (File file: files){
+			if (file.isDirectory()){
+				showFiles(file.listFiles());
+				
+			} else 
+			{
+				language1 = detectLanguage(file.getCanonicalPath());
+				String FileName=file.getCanonicalPath();
+				boolean keyPresent = languageMap.containsKey(language1);
+				if (keyPresent)
+				{
+					languageMap.get(language1).add(FileName);
+				}
+				else
+				{
+					ArrayList<String> fileNameList = new ArrayList<String>();
+					fileNameList.add(FileName);
+					languageMap.put(language1, fileNameList);
+				}
+				
+				for(String language1 : languageMap.keySet())
+				{
+					System.out.println("Language identified for the file " + file.getName() + " is : " + language1);
+					
+				}
+				
+			}
+			
+		}
 		
 		JSONArray langDetail = new JSONArray();
+		JSONObject[] language_obj = new JSONObject[languageMap.size()];
+		int i =0 ;
+		for(String language : languageMap.keySet())
+		{
+			language_obj[i] = new JSONObject();
+			language_obj[i].put("Language", language);
+			language_obj[i].put("Num", languageMap.get(language).size());
+			
+			langDetail.add(language_obj[i]);
+			i++;
+		}
 		
-		try (FileWriter file = new FileWriter("E:\\text_htmlfile1.json")) {
+		try (FileWriter file = new FileWriter("E:\\text_htmlfile1.json"))
+		{
 			file.write(langDetail.toJSONString());
 			System.out.println("Successfully Copied JSON Object to File...");
 			System.out.println("\nJSON Object: " + langDetail);
 		}
 		
 	}
-
 	
-	public static void showFiles(File[] files)throws Exception{
-		String language1 = null;
-		
-		for (File file: files){
-			if (file.isDirectory()){
-				//System.out.println("Directory : " + file.getName());
-				showFiles(file.listFiles());
-				
-			} else {
-				//System.out.println("File : " + file.getName());
-				//System.out.println("File Path : " + file.getCanonicalPath());
-				language1 = detectLanguage(file.getCanonicalPath());
-				//System.out.println("Language : " + language);
-				
-				}
-				
-			}
-			
-		
-		writeJson();
-		
-	}
-	
-	
-	
-	
-
-	
-	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception
+	{
 		File[] files = new File("E:\\text_html\\").listFiles();
 		showFiles(files);
 	}
